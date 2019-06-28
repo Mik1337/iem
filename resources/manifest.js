dfe24b5a9f882453bd0b75aa315217abba82db71
Mik <mik23@protonmail.ch>
2019-06-28 18:07:06 +0530

    manifest

---
+++ resources/manifest.js
@@ -0,0 +1,177 @@
+{
+  "btd": {
+      "title": "Basics Of Thermodynamics",
+      "paper": "https://drive.google.com/file/d/15G7qOg74C3R5juKiEktZnpau7562tOyL/view",
+      "notes": "",
+      "video": "",
+      "downloads": "",
+  },
+  "fm": {
+    "title": "Fluid Mechanics",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "mmm": {
+    "title": "Mechanical Measurement and Metrology",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "mom": {
+    "title": "Mechanics of Materials",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "mp1": {
+    "title": "Manufacturing Process 1",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "camd": {
+    "title": "Computer Aided Machine Drawings",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "kom": {
+    "title": "Kinematics of Machines",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "cadcam": {
+    "title": "Computer Aided Design and Manufacturing",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "mp2": {
+    "title": "Manufacturing Process 2",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "msm": {
+    "title": "Material Science and Metallurgy",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "me": {
+    "title": "Management and Entrepreneurship",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "wse": {
+    "title": "Work Study and Ergonomics",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "dme": {
+    "title": "Design of Machine Elements",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "se": {
+    "title": "Statistics for Engineers",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "ee": {
+    "title": "Engineering Economy",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "hrm": {
+    "title": "Human Resource Management",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "qar": {
+    "title": "Quality Assurance and Reliability",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "or": {
+    "title": "Operations Research",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "sma": {
+    "title": "Simulation Modeling and Analysis3",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "mm": {
+    "title": "Material Management",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "sem": {
+    "title": "Software Engineering and Management",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  },
+
+  "ve": {
+    "title": "Value Engineering",
+    "paper": "",
+    "notes": "",
+    "video": "",
+    "downloads": "",
+  }
+
+}
