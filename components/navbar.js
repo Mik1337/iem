7217992f7f39712511b26c565a1e651850d68d9b
Mik <mik23@protonmail.ch>
2019-07-26 02:06:43 +0530

    new file:   components/navbar.js 	new file:   pages/about.js 	new file:   pages/index.js 	deleted:    resources/about.html 	deleted:    resources/contribute.html 	deleted:    resources/faq.html 	deleted:    resources/index.html 	deleted:    resources/manifest.js 	deleted:    resources/reactapp.js 	deleted:    resources/stylesheet.css  Changes not staged for commit: 	deleted:    index.js

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+const NavBar = () => {
+  <div>
+    <ul>
+    <li><Link href='/'> Index </Link></li>
+     <li><Link href='/about'> About </Link></li>
+     // <li><a href="news.asp">News</a></li>
+     // <li><a href="contact.asp">Contact</a></li>
+     // <li><a href="about.asp">About</a></li>
+   </ul>
+ </div>
+}
+
+export default NavBar;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import NavBar from '../components/navbar'
+
+const Index = () => {
+  <div>
+    <h1>Hello not Next.js 👋</h1>
+  </div>
+}
+
+export default Index();
